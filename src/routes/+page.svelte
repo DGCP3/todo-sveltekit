0f53960755364d7eb496a0c2f4780667ae876310
Dagmawi Gezachew <zdagmawig1@gmail.com>
2023-10-10 12:44:21 +0300

    the big bang

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<img src="/test.svg" alt="test" style="aspect-ratio: 19/9;" />
